ea48435191fd97dbe4645ad97e1a947401329826
Lorena Salaises <lorena.salaises@gmail.com>
2025-06-20 17:58:29 -0400

    improved User entity to support the users table schema

--- repository/src/main/java/com/ecomarket/repository/entities/User.java
+++ repository/src/main/java/com/ecomarket/repository/entities/User.java
@@ -1,10 +1,27 @@
 package com.ecomarket.repository.entities;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
     private Long id;
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(max = 255, message = "Username cannot exceed 255 characters")
     private String username;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    @Size(max = 255, message = "Email cannot exceed 255 characters")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(max = 255, message = "Password cannot exceed 255 characters")
     private String password;
+
+    @NotBlank(message = "Role cannot be empty")
+    @Size(max = 20, message = "Role cannot exceed 20 characters")
     private String role;
 
     // Getters and Setter
